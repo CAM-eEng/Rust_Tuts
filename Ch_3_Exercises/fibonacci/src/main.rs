c30d63bee8f3ebe64b41dbc17267aab45953bbb8
Cameron Hartman <chart081@uottawa.ca>
2022-12-20 13:24:32 -0800

    Added fibonacci ex. Learned new match case "_ =>".

--- Ch_3_Exercises/fibonacci/src/main.rs
+++ Ch_3_Exercises/fibonacci/src/main.rs
@@ -1,11 +1,17 @@
+use std::io;
+
 fn main() {
     println!("Please enter the index of the Fibonacci sequence you wish to calculate.");
     let mut index = String::new();
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read input");
-    
-    if index == 0{
-        
+    io::stdin().read_line(&mut index).expect("Failed to read input");
+    let index_val: u64 = index.trim().parse().expect("Please type a number!");
+    println!("The fibonnaci number for {} is {}", index, fibo(index_val));
+}
+
+fn fibo(n: u64) -> u64 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => fibo(n-1) + fibo(n-2),
     }
 }
